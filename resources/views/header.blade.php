a758b4b608c31628b90849bd5c03eba09b81a263
ItShimm <bogdan89228543868@gmail.com>
2021-07-09 01:02:17 +0500

    navbar fix

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,6 +5,9 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="">NavBar</a>
+        <div class="nav-item d-sm-block d-md-block d-lg-none">
+            <i class="bi bi-telephone-fill"></i>
+        </div>
         <div class="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo03">
             <ul class="navbar-nav  mb-2 mb-lg-0">
                 <li class="nav-item m-2">
@@ -21,7 +24,7 @@
                 </li>
             </ul>
         </div>
-        <div class="nav-item">
+        <div class="nav-item d-none d-lg-block">
             <i class="bi bi-telephone-fill"></i> +0 000-000-00-00 <br>
             <i class="bi bi-geo-alt-fill"></i> Верхнее тушево 1 <br>
             <i class="bi bi-envelope"></i> bestcompany@mail.ru
