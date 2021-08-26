9f81687bf535ccf892c27017f7d0c453d848422c
ItShimm <bogdan89228543868@gmail.com>
2021-08-27 00:49:03 +0500

    fix title filter

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -102,9 +102,9 @@
                 <div class="row pt-sm-3 pt-lg-0">
                     <div class="@if ($news->isNotEmpty()) col-lg-10 @else col-lg-12 @endif col-md-12 mb-4">
                         <div class="filter-site pt-3 pb-5 px-2">
-                            <div class="text-white row my-4">
-                                <div class="col-5 text-end"><i class="fas fa-search fa-2x"></i></div>
-                                <div class="col-7 text-left"><span class="h3 pl-5">Подобрать круиз</span></div>
+                            <div class="text-white my-4 d-flex justify-content-center">
+                                <div class=" text-end px-2"><i class="fas fa-search fa-2x"></i></div>
+                                <div class=" text-left px-2"><span class="h3 pl-5">Подобрать круиз</span></div>
                             </div>
                             <div>
                                 <div id="riverlines" data-key="f09e2d456b55d09d6db622c42f5e67237779f355" data-id="1686"></div>
